1d305f5c50a300ca83f11aaba31c5b4c5d9d6bb6
Karol Wypchlo <kwypchlo@gmail.com>
2024-02-19 16:51:58 +0100

    min 1 review

--- src/utils/generators.ts
+++ src/utils/generators.ts
@@ -27,7 +27,7 @@ export const generateReview = (productId: string, authorId: string) => {
 
 export const generateProductReviews = (productId: string, authorIds: string[]) => {
   authorIds = authorIds.slice(); // copy the array
-  return new Array(faker.number.int({ min: 0, max: 50 })).fill(null).map(() => {
+  return new Array(faker.number.int({ min: 1, max: 50 })).fill(null).map(() => {
     faker.helpers.shuffle(authorIds, { inplace: true }); // shuffle the array in place
 
     return generateReview(productId, authorIds.pop()!);
